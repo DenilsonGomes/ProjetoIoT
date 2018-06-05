f2666131254c163edc34ae34536df394730019cf
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-05 11:54:37 -0300

    Codigo para monitorar Temperatura e Humidade

--- disp1/disp1.ino
+++ disp1/disp1.ino
@@ -11,32 +11,29 @@
 //--Variaveis Globais--//
 #define DHTPIN 2 // Pino do DHT
 #define DHTTYPE DHT11 // Definindo o sensor DHT11
+#define LDR A0 // Pino do LDR
+int valorLDR = 0;
 
 //--Instanciando Objetos--//
 DHT dht(DHTPIN, DHTTYPE, 15); // Inicializando o sensor DHT
 
  void setup() {
-  Serial.begin(9600);
-  // Iniciando o DHT 
-  dht.begin();
+  Serial.begin(9600); //Inicia comunicação serial
+  dht.begin(); // Iniciando o DHT 
 
 }
 
 void loop() {
-  // Lendo a umidade
-  int u = dht.readHumidity();
-
-  //Verificando se houve falha na leitura
-  if(u == 2147483647){
-    delay(10);
-    return;
-  }
+  int u = dht.readHumidity(); // Lendo a umidade
+  if(u == 2147483647){delay(10);return;} //Verificando se houve falha na leitura
   
-  // Lendo a temperatura em graus Celsius
-  int t = dht.readTemperature();
+  int t = dht.readTemperature(); // Lendo a temperatura em graus Celsius
 
   Serial.println(String("Humidade: ") + String(u));
   Serial.println(String("Temperatura: ") + String(t));
 
+  valorLDR = analogRead(LDR); // Atribui o valor da luminosidade no LDR1 para valorSensor1
+  Serial.println(String("Luminosidade: ") + String(valorLDR));  
+  
   delay(3000);
 }
