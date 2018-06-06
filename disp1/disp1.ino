50d7739d2fc1fbd66ae3f4d0f6e237370ecd4d73
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-06 12:05:01 -0300

    Codigo para monitorar Temperatura e Humidade

--- disp1/disp1.ino
+++ disp1/disp1.ino
@@ -9,31 +9,46 @@
 #include "DHT.h"
 
 //--Variaveis Globais--//
-#define DHTPIN 2 // Pino do DHT
+#define pinDHT 2 // Pino do DHT
 #define DHTTYPE DHT11 // Definindo o sensor DHT11
-#define LDR A0 // Pino do LDR
-int valorLDR = 0;
+#define pinLDR A0 // Pino do LDR
+int valorLDR = 0; //Variavel para sinal do LDR
+#define pinSound A1 // Pino do sensor de som
+int valorSOM = 0; //Variavel para sinal do sensor de som
+#define pinPIR 3 // Pino do sensor de presença PIR
+bool valorPIR;
 
 //--Instanciando Objetos--//
-DHT dht(DHTPIN, DHTTYPE, 15); // Inicializando o sensor DHT
+DHT dht(pinDHT, DHTTYPE, 15); // Inicializando o sensor DHT
 
  void setup() {
   Serial.begin(9600); //Inicia comunicação serial
   dht.begin(); // Iniciando o DHT 
-
+  pinMode(pinLDR, INPUT);
+  pinMode(pinSound, INPUT);
+  pinMode(pinPIR, INPUT);
 }
 
 void loop() {
   int u = dht.readHumidity(); // Lendo a umidade
   if(u == 2147483647){delay(10);return;} //Verificando se houve falha na leitura
-  
   int t = dht.readTemperature(); // Lendo a temperatura em graus Celsius
 
   Serial.println(String("Humidade: ") + String(u));
   Serial.println(String("Temperatura: ") + String(t));
 
-  valorLDR = analogRead(LDR); // Atribui o valor da luminosidade no LDR1 para valorSensor1
+  valorLDR = analogRead(pinLDR); // Atribui o valor da luminosidade no LDR para valorLDR
   Serial.println(String("Luminosidade: ") + String(valorLDR));  
-  
-  delay(3000);
+
+  valorSOM = analogRead(pinSound); // Atribui o valor de som para valorSOM
+  Serial.println(String("Som: ") + String(valorSOM));
+
+  valorPIR = digitalRead(pinPIR); // Atribui o valor da leitura digital de pinPIR para valorPIR
+  if(valorPIR){
+    Serial.println("Presença detectada");
+  }else{
+    Serial.println("Presença não detectada");
+  }
+  Serial.println(valorPIR);
+  delay(8000);
 }
