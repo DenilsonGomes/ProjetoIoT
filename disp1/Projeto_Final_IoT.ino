d2dad0e2ead2a454d084987bf70b08a6a6c5ffd2
Denilson Gomes <denilsongomesdg97@gmail.com>
2018-06-19 11:34:36 -0300

    Codigos atualizados

--- disp1/Projeto_Final_IoT.ino
+++ disp1/Projeto_Final_IoT.ino
@@ -1,21 +1,21 @@
 /*
  * Autores: Denilson Gomes Vaz da Silva, Raphael Carneiro de Freitas
- * Graduando em Engenharia da Computação
+ * Graduandos em Engenharia da Computação
  * Projeto de Internet das Coisas (IoT) - Prof. Dr. Wendley Silva
  * Codigo para monitorar 3 ou mais variaveis
  */
 
 
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h> // Biblioteca usada, baixe e instale se não a tiver, link abaixo
-#include <DHT.h>                          //https://github.com/knolleary/pubsubclient/blob/master/examples/mqtt_esp8266/mqtt_esp8266.ino
+#include <PubSubClient.h>
+#include <DHT.h>                          
 
 //Define a pinagem do ESP8266
 
-#define D0    16 //pino do led
-#define D1    5 //pino do sensor de som
-#define D2    4 // pino dht11
-#define D3    0
+#define D0    16 //Pino do led
+#define D1    5 
+#define D2    4  // Pino dht11
+#define D3    0  // Pino do Sensor de Presença
 #define D4    2
 #define D5    14
 #define D6    12
@@ -26,10 +26,9 @@
 
 #define pinPIR D3 // Pino do sensor de presença PIR
 #define pinSound D1 // Pino do sensor de som
-
 #define DHTTYPE DHT11
 
-DHT dht(D2, DHTTYPE, 15);
+DHT dht(D2, DHTTYPE);
 bool valorPIR;
 int valorSOM = 0; //Variavel para sinal do sensor de som
  
@@ -39,13 +38,11 @@ const char* mqttServer = "m10.cloudmqtt.com"; // Aqui o endereço do seu servido
 const int mqttPort =12217; // Aqui mude para sua porta fornecida pelo site
 const char* mqttUser = "zpwwnbka"; //  Aqui o nome de usuario fornecido pelo site
 const char* mqttPassword = "vgNRAVKehHWL"; //  Aqui sua senha fornecida pelo site
-//char EstadoSaida = '0';  
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-
 void mqtt_callback(char* topic, byte* payload, unsigned int length);
-
 
 void setup() {
     dht.begin();
@@ -55,12 +52,13 @@ void setup() {
     digitalWrite(D2, LOW);
     pinMode(pinPIR, INPUT);
     digitalWrite(pinPIR, LOW);
-    //pinMode(D2, OUTPUT);
-    //digitalWrite(D2, LOW);  
+    pinMode(D8, OUTPUT);
+    digitalWrite(D8, LOW);
+    
 
- 
   Serial.begin(115200);
- 
+
+ //------Conecta a rede Wi-Fi
   WiFi.begin(ssid, password);
  
   while (WiFi.status() != WL_CONNECTED) {
@@ -68,80 +66,86 @@ void setup() {
     Serial.println("Connecting to WiFi..");
   }
   Serial.println("Connected to the WiFi network");
- 
+
+ //------Conecta ao Broker MQTT
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
  
   while (!client.connected()) {
     Serial.println("Connecting to MQTT...");
- 
     if (client.connect("ESP8266Client", mqttUser, mqttPassword )) {
- 
       Serial.println("connected");  
- 
     } else {
- 
       Serial.print("failed with state ");
       Serial.print(client.state());
-      delay(2000);
- 
+      delay(2000); 
     }
   }
   
- 
+ //Topicos nos quais o dispositivo está inscrito
   client.publish("LEDcor","#ffffff");
   client.subscribe("LED");
   client.subscribe("PRES");
-  client.subscribe("SOM");
   client.subscribe("TEMP");
   client.subscribe("UMID");
   client.subscribe("LEDcor");
+  client.subscribe("LUZ");
+  client.subscribe("ACESSO");
+  
  
 }
  // LENDO A TEMPERATURA E PUBLICANDO
     void leTemperatura(void){
-     // digitalRead(D2);
+    // while(1){
       int t = dht.readTemperature();
       char t2[3];
       String str =String(t);
       str.toCharArray(t2,3);
-       client.publish("TEMP",t2);
-      // delay(20000);     
+      client.publish("TEMP",t2);
+      //delay(20000);
+     //}
     }
     
  // LENDO A UMIDADE E PUBLICANDO
     void leUmidade(void){
-     // digitalRead(D2);
+    // while(1){
       int u = dht.readHumidity();
       char u2[3];
-      String str =String(u);
-      str.toCharArray(u2,3);
-       client.publish("UMID",u2);
-      // delay(20000);
+      String str2 =String(u);
+      str2.toCharArray(u2,3);
+      client.publish("UMID",u2);
+      //delay(20000);
+     //}
     }
 
 // DETECTANDO PRESENÇA E PUBLICANDO
     void detectaPres(void){
+      //while(1){
      valorPIR = digitalRead(pinPIR); // Atribui o valor da leitura digital de pinPIR para valorPIR
         if(valorPIR){
           Serial.println("Presença detectada");
-          client.publish("PRES","PRESENÇA DETECTADA");
+          client.publish("PRES","DETECTADA");
        }else{
           Serial.println("Presença não detectada");
-          client.publish("PRES","PRESENÇA NÃO DETECTADA");
+          client.publish("PRES","NAO DETECTADA");
           }
+        //  delay(20000);
+      //}
     }
  
- // DETECTANDO SOM E PUBLICANDO
-    void detectaSom(void){
-        valorSOM = digitalRead(pinSound); // Atribui o valor de som para valorSOM
-        Serial.println(String("Som: ") + String(valorSOM));
-        char som[3];
-        String strsom =String(valorSOM);
-        strsom.toCharArray(som,3);
-        client.publish("SOM",som);
-    }
-    
+ // DETECTANDO LUZ E PUBLICANDO
+    void detectaLuz(void){
+     // while(1){
+      int intensidade = analogRead(A0);   // read the input on analog pin 0
+      char v2[5];
+      String str3=String(intensidade);
+      str3.toCharArray(v2,5);
+      Serial.println(intensidade);   // print out the value you read
+      client.publish("LUZ",v2);
+      //delay(20000);
+      //}
+}
+        
     
 void callback(char* topic, byte* payload, unsigned int length) {
  
@@ -151,8 +155,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Mensagem:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
-
-       String msg;
+     String msg;
     
  
     //obtem a string do payload recebido
@@ -162,52 +165,48 @@ void callback(char* topic, byte* payload, unsigned int length) {
        msg += c;
     }
   
-
-        
-    //toma ação dependendo da string recebida:
-    //verifica se deve colocar nivel alto de tensão na saída.
-    //IMPORTANTE: o Led já contido na placa é acionado com lógica invertida (ou seja,
-    //enviar HIGH para o output faz o Led apagar / enviar LOW faz o Led acender)
- 
- 
-    //verifica se deve colocar nivel alto de tensão na saída se enviar L e digito, ou nivel baixo se enviar D e digito no topíco LED
-
+    //verifica as mensagens recebidas e ações a serem tomadas.
     
-       if (msg.equals("L0"))
+       if (msg.equals("L0"))// Liga o Led
     {
         digitalWrite(D0, HIGH);
         client.publish("LEDcor","#ffff00");
-        
-       // EstadoSaida = '0';
     }
-       if (msg.equals("D0"))
+       if (msg.equals("D0"))// Desliga o Led
     {
         digitalWrite(D0, LOW);
          client.publish("LEDcor","#ffffff");
-       // EstadoSaida = '0';
     }
-         if (msg.equals("l1"))
+
+       if (msg.equals("ATIVASISTEMA"))//Ativa o Sistema
     {
-       leTemperatura();
+        digitalWrite(D8, HIGH);    
     }
-         if (msg.equals("l2"))
+       if (msg.equals("DESLIGASISTEMA"))//Desliga o Sistema
+    {
+        digitalWrite(D8, LOW);
+         client.publish("LEDcor","#ffffff");
+    }
+    
+         if (msg.equals("l1"))
+    {   
+       leTemperatura();// Lê a Temperatura 
+    }
+        
+         if (msg.equals("l2"))// Lê a Umidade
     {
        leUmidade();
     }
-         if (msg.equals("l3"))
+         if (msg.equals("l3"))//Lê o sensor de presença
     {
        detectaPres();
     }
-        if (msg.equals("l4"))
+        if (msg.equals("l4"))// Lê o sensor de Luz
     {
-       detectaSom();
+       detectaLuz();
     }
     
-
-       
-    
-     }
-   
+     }//fecha o for
  
   Serial.println();
   Serial.println("-----------------------------");
@@ -215,7 +214,5 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
  
 void loop() {
-  client.loop();
-  
-  
+  client.loop();  
 }
--- disp2/ESP8266_RFID_CONTROLE_ACESSO.ino
+++ disp2/ESP8266_RFID_CONTROLE_ACESSO.ino
@@ -1,13 +1,17 @@
- 
+/*
+ * Autores: Denilson Gomes Vaz da Silva, Raphael Carneiro de Freitas
+ * Graduandos em Engenharia da Computação
+ * Projeto de Internet das Coisas (IoT) - Prof. Dr. Wendley Silva
+
  ************************************************************************************************************
  *************************PINAGEM****************************************************************************
   
-  RFID-RC522   Wemos          Nodemcu
-RST/Reset RST  D3  [1*]        D3 [1*]      GPIO-0  [1*]
-SPI SS  SDA[3] D8  [2*]        D8 [2*]      GPIO-15 [2*]
-SPI MOSI MOSI  D7              D7           GPIO-13
-SPI MISO MISO  D6              D6           GPIO-12
-SPI SCK SCK    D5              D5           GPIO-14
+  RFID-RC522          Nodemcu
+RST/Reset RST          D3 [1*]      GPIO-0  [1*]
+SPI SS  SDA[3]         D8 [2*]      GPIO-15 [2*]
+SPI MOSI MOSI          D7           GPIO-13
+SPI MISO MISO          D6           GPIO-12
+SPI SCK SCK            D5           GPIO-14
 
 [1*] (1, 2) Configuracao tipica definindo como RST_PIN no sketch / programa.
 [2*] (1, 2) Configuracao, tipica definindo como SS_PIN no sketch / programa.
@@ -18,27 +22,87 @@ SPI SCK SCK    D5              D5           GPIO-14
 
 #include <SPI.h>
 #include <MFRC522.h>
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
 
 #define RST_PIN    D3    
 #define SS_PIN     D8   
 #define LED        D1
 
-MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
+const char* ssid = "ap201"; //Aqui o nome da sua rede local wi fi
+const char* password =  "solpa050709"; // Aqui a senha da sua rede local wi fi
+const char* mqttServer = "m10.cloudmqtt.com"; // Aqui o endereço do seu servidor fornecido pelo site 
+const int mqttPort =12217; // Aqui mude para sua porta fornecida pelo site
+const char* mqttUser = "zpwwnbka"; //  Aqui o nome de usuario fornecido pelo site
+const char* mqttPassword = "vgNRAVKehHWL"; //  Aqui sua senha fornecida pelo site
+//char EstadoSaida = '0';  
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length);
+
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Cria uma instancia MFRC522
 
 void setup() 
 {
   Serial.begin(9600);   // Inicia a serial
+
+//-----------Conecta a rede Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+//----------Conecta ao Broker MQTT
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT..."); 
+    if (client.connect("ESP8266Client2", mqttUser, mqttPassword )) {
+      Serial.println("connected");  
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+  
   SPI.begin();      // Inicia  SPI bus
   mfrc522.PCD_Init();   // Inicia MFRC522
   Serial.println("Aproxime o seu cartao do leitor...");
   Serial.println();
-  //digitalWrite(D1,LOW);
   pinMode(D1, OUTPUT);
   pinMode(D0, OUTPUT); 
+
+//Topicos nos quais o dispositivo está inscrito
+  client.subscribe("ACESSO");
+  client.subscribe("LED");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Mensagem recebida no topico: ");
+  Serial.println(topic);
+  Serial.print("Mensagem:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+       String msg;  
+ 
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+  }
 }
 
 void loop() 
 {
+  client.loop();
   // Procura por cartao RFID
   if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
@@ -64,16 +128,17 @@ void loop()
   Serial.print("Mensagem : ");
   conteudo.toUpperCase();
 
-  //if (conteudo.substring(1) == "01 D1 FC 52" || "03 1C A0 F6") //UID 1 - Cartao
-  if (conteudo.substring(1) == "BB 59 8B 89") //UID 1 - Cartao
+  if (conteudo.substring(1) == "BB 59 8B 89") // Cartao cujo UID será reonhecido
   {
     Serial.println("Liberado!");
     Serial.println();
-    digitalWrite(D1, HIGH);     // LIGA LED OU/ ativa rele, abre trava solenoide
-    delay(200);              // DELAY /espera 3 segundos
-    digitalWrite(D1, LOW);  // DESlIGA LED OU /desativa rele, fecha  trava solenoide
-    digitalWrite(D0, HIGH);//Ativa led que indica que o sistema está funcionando 
+    digitalWrite(D1, HIGH);     // Liga o buzzer que indica cartão reconhecido
+    delay(200);                 // espera 200 milissegundos
+    digitalWrite(D1, LOW);      // Desliga o buzzer - (1 bip)
+    digitalWrite(D0, HIGH);     // Liga o led que indica usuário autenticado 
+    client.publish("ACESSO","ATIVASISTEMA"); //Envia uma mensagem ao broker para ligar o sistema
     delay(2000);//Espera dois segundos para ler novamente
+    
   } else{
     Serial.println("Acesso Negado!");
     digitalWrite(D1, HIGH);     
@@ -84,9 +149,11 @@ void loop()
     delay(100);              
     digitalWrite(D1, LOW);  
     delay(100);
-    digitalWrite(D1, HIGH);     
+    digitalWrite(D1, HIGH); // Três bips rapidos para informar que o cartão não foi reconhecido    
     delay(100);              
-    digitalWrite(D1, LOW);  
+    digitalWrite(D1, LOW);
+    digitalWrite(D0, LOW);//Desativa led que indica que o sistema está funcionando  
+    client.publish("ACESSO","DESLIGASISTEMA"); // Publica a mensagem no broker para desligar o sistema
     delay(2000);//Espera dois segundos para ler novamente
     }
-}
+}// fim do loop
